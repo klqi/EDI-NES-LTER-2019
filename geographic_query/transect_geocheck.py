3b1104ac8e2a1b577f9b0df598f51ea99062d6c8
Kathy Qi <klqi@ucsd.edu>
2019-07-09 14:06:19 -0400

    Changed vincenty to geodesic and fixed comments

--- geographic_query/transect_geocheck.py
+++ geographic_query/transect_geocheck.py
@@ -3,6 +3,7 @@
 # File header: Tool to check given files to verify whether given samples are
 # valid within the complete transect line. Also can choose certain samples
 # within a certain distance to output to a separate csv file
+# Requirements: pandas, folium, geopy.distance, request, ssl
 # Still under local development so needs clean up to accomodate more user
 # interaction
 
@@ -21,7 +22,7 @@ urllib.request.urlopen(underway_file)
 # coordinates for station 5
 st5 = (40.5133, -70.8833)
 st5_long = -70.8833
-# show station 5 as main marker
+# add station 5 as main marker
 map = folium.Map(location=st5, zoom_start=14)
 folium.Marker(st5, popup='<i>Station 5</i>').add_to(map)
 # initialize data frames
@@ -39,10 +40,12 @@ underway['date'] = pd.Series(underway['date']).dt.round("min")
 all_samples = pd.merge(all_samples, underway, how='left', left_on='date', right_on='date')
 all_samples['offset'] = 0
 subset_samples = pd.read_csv(subset_file, usecols=['sample_identifier'])
-# samples within latitudinal range
+# initialize column headers with latitude/longitude from original file and
+# ulatitude/ulongitude from gps_furuno data
 columns = ['index', 'sample_identifier', 'latitude', 'longitude', 'ulatitude', 'ulongitude']
 col = ['date', 'new_pid']
 good_samples = pd.DataFrame(columns=columns)
+# only includes data from underway for query
 joes_samples = pd.DataFrame(columns=col)
 # prompt user input to specify distance from longitude
 ref_dist = float(input('Please enter an distance (km): '))
@@ -65,14 +68,15 @@ for row in all_samples.iterrows():
     # markers for all samples on maps
     # folium.CircleMarker(coords, radius=1, color='green').add_to(map)
     folium.CircleMarker(ucoords, radius=1, color='crimson').add_to(map)
-    # get sample identifier
+    # get sample identifier from last 24 characters of pid string
     whole = all_samples.pid[counter]
     cut = whole[-24:]
     all_samples.key[counter] = cut
     # only plot marker if sample is 1 km east/west from station 5
-    dist = geopy.distance.vincenty(ref_coord, coords).km
-    udist = geopy.distance.vincenty(ref_coord, ucoords).km
-    off_dist = geopy.distance.vincenty(ucoords, coords).m
+    dist = geopy.distance.geodesic(ref_coord, coords).km
+    udist = geopy.distance.geodesic(uref_coord, ucoords).km
+    # offset distance from dist and udist
+    off_dist = geopy.distance.geodesic(ucoords, coords).m
     all_samples.offset[counter] = off_dist
     # if both coordinates are within the ref_dist range
     if (dist <= ref_dist and udist <= ref_dist):
@@ -99,7 +103,7 @@ for row in all_samples.iterrows():
         folium.CircleMarker(ucoords, radius=1, color='yellow').add_to(map)
         # put into coordinates with sample identifier in dataframe
         good_samples = good_samples.append({'index': index, 'sample_identifier': cut, 'latitude': 'NA', 'longitude': 'NA', 'ulatitude': ulat, 'ulongitude': ulong}, ignore_index=True)
-        # put coordinates into joes_samples
+        # put coordinates into joes_samples, only including underway data
         joes_samples = joes_samples.append({'date': all_samples.date[counter], 'new_pid': all_samples.pid[counter]}, ignore_index=True)
         # keep track of counter
         index += 1
@@ -114,18 +118,18 @@ joes_samples['date'] = pd.to_datetime(joes_samples['date'])
 joes_samples = pd.merge(joes_samples, old_pids, how='outer', on='date')
 joes_samples = joes_samples.replace(r'^\s*$', "NA", regex=True)
 joes_samples.rename(columns={'pid': 'old_pids'}, inplace=True)
-# plot coordinates from oriignla subset sample onto map- quality check portion
+# plot coordinates from original subset sample onto map- quality check portion
 counter = 0
 '''for row in subset_samples.iterrows():
-    lat = subset_samples.latitude[counter]
-    long = subset_samples.longitude[counter]
+    lat = subset_samples.gps_furuno_latitude[counter]
+    long = subset_samples.gps_furuno_longitude[counter]
     counter += 1
     # get coordinates
-    coords = (lat, long)
+    ucoords = (lat, long)
     # Add marker to original subset coordinates
-    folium.CircleMarker(coords, radius=1, color='purple').add_to(map)'''
+    folium.CircleMarker(ucoords, radius=1, color='purple').add_to(map)'''
 # save map
-map.save("mymap.html")
+map.save("IFCB_EN608_map.html")
 # save information to csv files, comment out as necessary
 good_samples.to_csv("good_transect_subset.csv", index=None, header=True)
 subset_samples.to_csv("transect_subset.csv", index=None, header=True)
