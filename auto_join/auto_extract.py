be097c2817b41bf8285bd4f0969a5a2515ac3999
Kathy Qi <klqi@ucsd.edu>
2019-07-16 13:02:40 -0400

    Added functionality to join ties in automated classifications

--- auto_join/auto_extract.py
+++ auto_join/auto_extract.py
@@ -5,14 +5,16 @@
 import pandas as pd
 import ssl
 import urllib
-import tkinter as tk
+'''import tkinter as tk
 from tkinter import filedialog
+'''
 
 
 def merge_frames(f_df, c_df, id_df):
     # get column with highest number (still need to implement tiebreaker)
     rem = c_df.iloc[:, 1:]
-    c_df['winner'] = rem.idxmax(axis=1)
+    winner = rem.idxmax(axis=1)
+    c_df['winner'] = winner
     # make substring key for matching
     c_df.pid = c_df.pid.str.slice(25, 30)
     c_df.pid = c_df.pid.str.lstrip("0")  # gets rid of leading zeros
@@ -20,18 +22,35 @@ def merge_frames(f_df, c_df, id_df):
     c_df.pid = pd.to_numeric(c_df.pid, downcast='integer')
     # only use most_likely and pid
     c_df = c_df[['pid', 'winner']]
-    # merge c_df and f_df using pid/roi_number as a key
-    cnc_df = pd.merge(f_df, c_df, how='left', left_on='roi_number', right_on='pid')
-    # drop duplicate row
+    # merge class and id files first
+    cid_df = pd.merge(c_df, id_df, how='left', left_on='winner', right_on='name')
+    # then merge features and cid_df
+    cnc_df = pd.merge(f_df, cid_df, how='left', left_on='roi_number', right_on='pid')
+    # drop duplicate column
     cnc_df = cnc_df.drop('pid', axis=1)
-    # merge data frame based on winner to get ids
-    cnc_df = pd.merge(cnc_df, id_df, how='left', left_on='winner', right_on='name')
-    return cnc_df
+    # make sure grouped columns are all strings
+    cnc_df[['winner', 'name', 'id', 'international_id']] = cnc_df[['winner', 'name', 'id', 'international_id']].astype(str)
+    # group columns by ties, keeping original columns
+    winners = cnc_df.groupby(
+        ['sample_identifier', 'roi_number']).agg(
+                {
+                    'winner': '|'.join,
+                    'name': '|'.join,
+                    'id': '|'.join,
+                    'international_id': '|'.join,
+                    'Area': 'first',
+                    'Biovolume': 'first',
+                    'EquivDiameter': 'first',
+                    'MajorAxisLength': 'first',
+                    'MinorAxisLength': 'first'
+                }
+            ).reset_index()
+    return winners
 
 
 # pd.read_csv(f_url) will loop through all urls, use one for now for testing
 # files to check
-root = tk.Tk()
+'''root = tk.Tk()
 # gets rid of annoying window
 root.withdraw()
 # close window when done
@@ -41,15 +60,17 @@ root.lift()
 root.attributes('-topmost', True)
 root.after_idle(root.attributes, '-topmost', False)
 # prompt user input for all samples file
-file_name = filedialog.askopenfilename(initialdir=".", title="Choose csv file with all samples")
+file_name = filedialog.askopenfilename(initialdir=".", title="Choose csv file with all samples")'''
+file_name = "use_for_testing.csv"
 # read in file with ids
 classid_file = "20190529_classify_classlabel.csv"
 # initialize samples data frame
 samples = pd.read_csv(file_name, usecols=["new_pid"])
 # initialize features and class data frames
-columns = ['roi_number', 'Area', 'Biovolume', 'EquivDiameter', 'MajorAxisLength', 'MinorAxisLength']
+columns = ['sample_identifier', 'roi_number', 'Area', 'Biovolume', 'EquivDiameter', 'MajorAxisLength', 'MinorAxisLength']
 #  cnc_df = pd.DataFrame(columns=columns)
 f_df = pd.DataFrame(columns=columns)
+# initialize counter and loop through all samples
 counter = 0
 for row in samples.iterrows():
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -59,9 +80,10 @@ for row in samples.iterrows():
     class_file = '{}_class_scores.csv'.format(samples.new_pid[counter])
     # extract ROI ID, class, area, biovolume, major/min axes
     temp_ft = pd.read_csv(features_file, usecols=['roi_number', 'Area', 'Biovolume', 'EquivDiameter', 'MajorAxisLength', 'MinorAxisLength'])
+    temp_ft['sample_identifier'] = samples.new_pid[counter]
     temp_cl = pd.read_csv(class_file)
     # append to end of f_df and c_df to be merged later
-    f_df = pd.concat([f_df, temp_ft], ignore_index=True)
+    f_df = pd.concat([f_df, temp_ft], ignore_index=True, sort=True)
     if (counter == 0):
         c_df = temp_cl
     else:
