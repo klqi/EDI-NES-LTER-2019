e9966bb646d00fb91ae67a0f7b929ea05a2221f4
Kathy Qi <klqi@ucsd.edu>
2019-08-05 19:34:30 -0400

    Fixed some bash scripting stuff

--- plot2/exec.sh
+++ plot2/exec.sh
@@ -17,12 +17,12 @@ GEO_DIR="${BASE_DIR}/geographic_query/transect_geocheck.py"
 chmod u+x $GEO_DIR
 # change working directories (plot2 => geographic_query)
 cd geographic_query
-# run transect_geocheck.py to get comparison.csv with 1km range
+# run transect_geocheck.py to get geographic_subset.csv with 1km range
 python3 -W ignore $GEO_DIR <<EOF
 1
 EOF
 # copy desired output file to plot2 dir
-cp comparison.csv ../plot2
+cp geographic_subset.csv ../plot2
 ls
 
 # make sure auto_join script is executable
